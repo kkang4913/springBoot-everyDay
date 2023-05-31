122f1e4921af6f3a2b422d03d9fe01d1b17b2ee8
강승호 <kkang4913@naver.com>
2023-05-31 14:37:27 +0900

    First Commit

---
+++ src/test/java/com/web/springbootpro/SpringbootproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.springbootpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
